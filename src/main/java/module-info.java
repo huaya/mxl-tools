fe0e40cc23ea05b74c8e81896a35a7745d15cdf0
moore.ma <moore.ma@orderplus.com>
2021-08-18 13:56:46 +0800

    commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.maxlong.myfx1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.maxlong.myfx1 to javafx.fxml;
+    exports com.maxlong.myfx1;
+}
